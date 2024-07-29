0fea9396aa626ca8f7216fc4b53f856b90255d9c
kellyxc28 <kellyxiongc@gmail.com>
2024-07-29 15:59:34 -0400

    Completed: binarySearch()

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -30,6 +30,31 @@ const fibRec = (n) => {
 };
 
 // Return the index of target in arr, or -1 if not found using recursion
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {
+  if (start > end) return -1;
+
+  const mid = Math.floor((start + end) / 2);
+  if (arr[mid] === target) return mid;
+
+  // on the left side?
+  if (arr[mid] < target)
+    return binarySearch(arr, target, (start = mid + 1), end);
+
+  // on the right side?
+  if (arr[mid] > target)
+    return binarySearch(arr, target, start, (end = mid - 1));
+};
+
+// binarySearchIter
+// const binarySearch = (arr, target) => {
+//   let start = 0, end = arr.length - 1
+//   while (start <= end) {
+//       const mid = Math.floor((start + end) / 2)
+//       if (arr[mid] === target) return mid
+//       if (arr[mid] > target) end = mid - 1
+//       if (arr[mid] < target) start = mid + 1
+//   }
+//   return -1
+// };
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
